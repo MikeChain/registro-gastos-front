71af42bc168c452afd558b9d4bc100450dc8bc45
Miguel Angel Cadena Sanchez <miguelangel_cadena@outlook.com>
2020-11-16 20:00:58 -0600

    Auth context, estilos login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import { Header } from './components/Header';
 import { Login } from './components/Login';
 import { Toast } from './components/Toast';
+import { AuthProvider } from './context/AuthContext';
 import { ToastProvider } from './context/ToastContext';
 import './index.scss';
 
@@ -9,15 +10,15 @@ const App = () => {
 
   return (
     <ToastProvider>
+      <AuthProvider>
+        <Login />
 
-      <Login />
-
-      <Toast
-        position='bottom-right'
-        autoDelete={true}
-        dismissTime={3}
-      />
-
+        <Toast
+          position='bottom-right'
+          autoDelete={true}
+          dismissTime={3}
+        />
+      </AuthProvider>
     </ToastProvider>
   );
 }
